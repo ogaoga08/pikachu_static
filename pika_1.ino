f453351e9136e057a73963e458af8af00d224692
ogaoga08 <kamekameshin0514@gmail.com>
2025-01-26 23:57:28 +0900

    first commit

---
+++ pika_1.ino
@@ -0,0 +1,93 @@
+#include "M5StickCPlus2.h"
+
+int PinG26 = 26;
+int PinG36 = 36;
+
+// 音階
+#define NOTE_C4 261.626  //ド4 C4
+#define NOTE_C4b 277.183 //ド#4 C#4
+#define NOTE_D4 293.665  //レ4 D4
+#define NOTE_D4b 311.127 //レ#4 D#4
+#define NOTE_E4 329.628  //ミ4 E4
+#define NOTE_F4 349.228  //ファ4 F4
+#define NOTE_F4b 369.994 //ファ#4 F#4
+#define NOTE_G4 391.995  //ソ4 G4
+#define NOTE_G4b 415.305 //ソ#4 G#4
+#define NOTE_A4 440.000  //ラ4 A4
+#define NOTE_A4b 466.164 //ラ#4 A#4
+#define NOTE_B4 493.883  //シ4 B4
+#define NOTE_C5 523.251  //ド5 C5
+#define NOTE_C5b 554.365 //ド#5 C#5
+#define NOTE_D5 587.330  //レ5 D5
+#define NOTE_D5b 622.254 //レ#5 D#5
+#define NOTE_E5 659.255  //ミ5 E5
+#define NOTE_F5 698.456  //ファ5 F5
+#define NOTE_F5b 739.989 //ファ#5 F#5
+#define NOTE_G5 783.991  //ソ5 G5
+#define NOTE_G5b 830.609 //ソ#5 G#5
+#define NOTE_A5 880.000  //ラ5 A5
+#define NOTE_A5b 932.328 //ラ#5 A#5
+#define NOTE_B5 987.767  //シ5 B5
+#define NOTE_C6 1046.502 //ド6 C6
+
+//音符の長さ
+#define R_4 2800
+#define R_3 2100
+#define R_2 1400
+#define R_1 700
+#define R_1_2 350
+#define R_1_3 233.3
+#define R_1_4 175
+#define R_1_6 116.6
+#define R_1_8 87.5
+#define R_1_12 58.3
+#define R_1_16 43.75
+
+// ピカチュウの鳴き声のメロディ
+// float pikachu[][2] = {
+//   {NOTE_C5, R_1_4},  {NOTE_A4b, R_1_16}, {NOTE_A4, R_1_16},
+//   {NOTE_G4b, R_1_16}, {NOTE_G4, R_1_16}, {NOTE_F4b, R_1_16},
+//   {NOTE_E4, R_1_16}, {NOTE_D4b, R_1_16}, {NOTE_D4, R_1_16}, {NOTE_C4b, R_1_16},
+//   {NOTE_C4, R_1_4}
+// };
+float pikachu[][2] = {
+  {256, 100},  {515, 100}, {610, 150},
+  {452, 150}, {518, 150}
+};
+
+void setup() {
+  M5.Lcd.setFont(&fonts::lgfxJapanGothicP_32); 
+  StickCP2.begin();                  // ディスプレイを初期化
+  StickCP2.Lcd.setTextSize(1);       // フォントサイズを設定
+  StickCP2.Lcd.setRotation(0);       // 画面の向きを設定
+  StickCP2.Lcd.setTextColor(WHITE);  // テキストカラーを設定
+  M5.Speaker.setVolume(255);
+}
+
+void loop() {
+  int valG26 = analogRead(PinG26);  // G26ピンのアナログ値を取得
+  int valG36 = analogRead(PinG36);  // G36ピンのアナログ値を取得
+  
+  StickCP2.Lcd.fillScreen(BLACK);            // 画面をクリア
+  StickCP2.Lcd.setCursor(0, 0);              // カーソルを(0, 0)に移動
+  StickCP2.Lcd.printf("L:     %d\n", valG26);  // G26ピンのアナログ値を表示
+  StickCP2.Lcd.printf("R:     %d\n", valG36);  // G36ピンのアナログ値を表示
+
+  // G26またはG36が0の場合に音を鳴らす
+  if (valG26 == 0 || valG36 == 0) {
+    StickCP2.Lcd.fillScreen(YELLOW);  // ディスプレイを黄色で塗りつぶす
+    StickCP2.Lcd.setTextColor(BLACK); // テキストカラーを黒に設定
+    StickCP2.Lcd.setTextSize(1); 
+    StickCP2.Lcd.setCursor(20, 20);   // テキストの位置を設定
+    StickCP2.Lcd.printf("ピカァ"); // 日本語で「ピカァ！！」を表示
+    for (int i = 0; i < sizeof(pikachu) / sizeof(pikachu[0]); i++) {
+        M5.Speaker.tone(pikachu[i][0]);
+        delay(pikachu[i][1] * 0.8);  // 音符の長さの90%鳴らす
+        M5.Speaker.stop();           // 音を止める
+        delay(pikachu[i][1] * 0.1);  // 音符間の10%休止
+    }
+    M5.Speaker.stop();
+  }
+
+  delay(400);  // 繰り返しの間隔を設定
+}
